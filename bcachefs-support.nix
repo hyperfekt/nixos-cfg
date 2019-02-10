6517c21f8709d70a48f9900a55bfae79afee3b39
hyperfekt <git@hyperfekt.net>
2019-02-10 01:31:06 +0000

    update bcachefs

--- bcachefs-support.nix
+++ bcachefs-support.nix
@@ -1,19 +1,24 @@
-
 { pkgs, unstable, ...}:
 let
   fetchimport = args: ((import <nixos/nixpkgs> {config={};}).fetchurl args).outPath;
   kernel = unstable.linux_testing_bcachefs.override { argsOverride = {
-    version = "4.20.2019.02.08";
+    version = "4.20.2019.02.09";
     modDirVersion = "4.20.0";
     src = pkgs.fetchgit {
       url = "https://evilpiepirate.org/git/bcachefs.git";
-      rev = "474af5d4235d194d7299b68d2ebdd8fbbe291ed1";
-      sha256 = "1fkd03p21had22894pgccin4zx8vqlf623na6zdfirpy9ln6al6s";
+      rev = "09a546543006b60d44c4c51e7b40cd3ec7837a5e";
+      sha256 = "0p187vp9df0nnhawql0f2bj2sdim0f2b424106d41yxc8ayhz0d9";
     };
   }; };
   kernelPackages = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor kernel);
-  tools = unstable.bcachefs-tools.overrideAttrs (oldAttrs: {
-    name = "${oldAttrs.pname}-${oldAttrs.version}";
+  tools = unstable.bcachefs-tools.overrideAttrs (oldAttrs: rec {
+    name = "${oldAttrs.pname}-${version}";
+    src = pkgs.fetchgit {
+      url = "https://evilpiepirate.org/git/bcachefs-tools.git";
+      rev = "17c5215c1c542dd7b6b4f891a0da16d8c98e0591";
+      sha256 = "1zm2lnvijfmz483m2nhxz1rhk7ghgh0c450nyiwi6wa7lc1y3339";
+    };
+    version = "2019-02-09";
   });
 in
   {
@@ -22,8 +27,8 @@ in
     ];
     imports = [
       (fetchimport {
-        url = https://raw.githubusercontent.com/hyperfekt/nixpkgs/4dc2a73c1474cd4dac5ee46808a3cd8ba9f48b8a/nixos/modules/tasks/filesystems/bcachefs.nix;
-        sha256 = "0l5rap6kl5nmkfjpnkk906qrnp2w7z6gdv46yz8frydgyilsxbpw";
+        url = https://raw.githubusercontent.com/hyperfekt/nixpkgs/bcachefs-packageoptions/nixos/modules/tasks/filesystems/bcachefs.nix;
+        sha256 = "0p6kkh99282s90xjc4zir08ngvmf1lyzv841cqmisqsfryxqjli5";
       })
     ];
 
