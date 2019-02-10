91aa2f2fd20a549e870c7b9b8e7a08a98f82dcc6
hyperfekt <git@hyperfekt.net>
2019-02-10 00:53:34 +0000

    remove dependence of bcachefs-support on a fork with bcachefs update

--- bcachefs-support.nix
+++ bcachefs-support.nix
@@ -1,23 +1,34 @@
-{ pkgs, ...}:
-{
-  disabledModules = [ "tasks/filesystems/bcachefs.nix" ];
-  imports = [ (((import <nixos/nixpkgs> {config={};}).fetchurl { url = 
-https://raw.githubusercontent.com/hyperfekt/nixpkgs/4dc2a73c1474cd4dac5ee46808a3cd8ba9f48b8a/nixos/modules/tasks/filesystems/bcachefs.nix; 
-sha256 = "0l5rap6kl5nmkfjpnkk906qrnp2w7z6gdv46yz8frydgyilsxbpw"; }).outPath) ];
 
-  boot.bcachefs.toolPackage = (pkgs.callPackage (pkgs.fetchurl {url = 
-https://raw.githubusercontent.com/hyperfekt/nixpkgs/bcachefs-update/pkgs/tools/filesystems/bcachefs-tools/default.nix; 
-name = "bcachefs-tools-exp"; sha256 = "0kp2pdcy9x14nig4jq0gpagq9gwi8wli2x7rffr27v3864g8sj2v"; }) 
-{}).overrideAttrs (oldAttrs: {
+{ pkgs, unstable, ...}:
+let
+  fetchimport = args: ((import <nixos/nixpkgs> {config={};}).fetchurl args).outPath;
+  kernel = unstable.linux_testing_bcachefs.override { argsOverride = {
+    version = "4.20.2019.02.08";
+    modDirVersion = "4.20.0";
+    src = pkgs.fetchgit {
+      url = "https://evilpiepirate.org/git/bcachefs.git";
+      rev = "474af5d4235d194d7299b68d2ebdd8fbbe291ed1";
+      sha256 = "1fkd03p21had22894pgccin4zx8vqlf623na6zdfirpy9ln6al6s";
+    };
+  }; };
+  kernelPackages = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor kernel);
+  tools = unstable.bcachefs-tools.overrideAttrs (oldAttrs: {
     name = "${oldAttrs.pname}-${oldAttrs.version}";
   });
-  boot.kernelPackages = pkgs.lib.mkForce (pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor (pkgs.callPackage 
-(pkgs.fetchurl {url = 
-https://raw.githubusercontent.com/hyperfekt/nixpkgs/bcachefs-update/pkgs/os-specific/linux/kernel/linux-testing-bcachefs.nix; 
-name = "bcachefs-kernel-exp"; sha256 = "000rqy6nvs8f0j0nim7y3ikv3bpz3hv593b23cp83b7w6wb8f1xb"; }) {
-    kernelPatches = [
-      pkgs.kernelPatches.bridge_stp_helper
-      pkgs.kernelPatches.modinst_arg_list_too_long
+in
+  {
+    disabledModules = [
+      "tasks/filesystems/bcachefs.nix"
     ];
-  })));
-}
+    imports = [
+      (fetchimport {
+        url = https://raw.githubusercontent.com/hyperfekt/nixpkgs/4dc2a73c1474cd4dac5ee46808a3cd8ba9f48b8a/nixos/modules/tasks/filesystems/bcachefs.nix;
+        sha256 = "0l5rap6kl5nmkfjpnkk906qrnp2w7z6gdv46yz8frydgyilsxbpw";
+      })
+    ];
+
+    boot.bcachefs.toolPackage = tools;
+    boot.kernelPackages = pkgs.lib.mkForce kernelPackages;
+    boot.zfs.enableUnstable = true;
+    boot.supportedFilesystems = [ "bcachefs" ];
+  }
