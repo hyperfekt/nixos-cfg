3484c30e06d7b46d9bb07838d774be6cfd62f690
hyperfekt <git@hyperfekt.net>
2019-02-11 06:23:00 +0100

    add powersaving options

--- powersave.nix
+++ powersave.nix
@@ -1,1 +1,23 @@
-{}
+{lib, ...}:
+{
+  powerManagement = {
+    cpuFreqGovernor = "powersave";
+    # SATA link power management
+    scsiLinkPolicy = "med_power_with_dipm";
+  };
+
+  boot.extraModprobeConfig = lib.mkMerge [
+    # idle audio card after one second
+    "options snd_hda_intel power_save=1"
+    # enable wifi power saving (keep uapsd off to maintain low latencies)
+    "options iwlwifi power_save=1 power_leveL=5 d0i3_disable=0 uapsd_disable=1"
+    "options iwldvm force_cam=0"
+  ];
+
+  services.udev.extraRules = lib.mkMerge [
+    # autosuspend USB devices
+    ''ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto"''
+    # autosuspend PCI devices
+    ''ACTION=="add", SUBSYSTEM=="pci", TEST=="power/control", ATTR{power/control}="auto"''
+  ];
+}
