fba46ab648f1fffb2484618d12f7f1d73afc2057
hyperfekt <git@hyperfekt.net>
2019-02-18 21:38:08 +0100

    enable video acceleration

--- video-acceleration.nix
+++ video-acceleration.nix
@@ -1,1 +1,11 @@
-{}
+{ unstable, ... }:
+{
+  hardware.opengl = {
+     enable = true;
+     extraPackages = with unstable; [
+       (vaapiIntel.override { enableHybridCodec = true; })
+       vaapiVdpau
+       libvdpau-va-gl
+     ];
+   };
+}
