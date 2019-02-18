2c470dff010381b0e1de8fc2b47ef258398b2c93
hyperfekt <git@hyperfekt.net>
2019-02-18 21:35:42 +0100

    rebuild KDE start menu

--- kde.nix
+++ kde.nix
@@ -1,5 +1,6 @@
+{ pkgs, ...}:
 {
-    # Enable the X11 windowing system.
+  # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable touchpad support.
@@ -10,5 +11,13 @@
   services.xserver.desktopManager.plasma5 = {
     enable = true;
     enableQt4Support = false;
+  };
+
+  # see #20776
+  system.userActivationScripts.updateStartMenu = {
+    text = ''
+      ${pkgs.libsForQt5.kservice}/bin/kbuildsycoca5
+    '';
+    deps = [];
   };
 }
