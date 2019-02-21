38b91ef655a2a8c2854187c886ce63938ab8d4e7
hyperfekt <git@hyperfekt.net>
2019-02-21 11:25:17 +0100

    backup: set file permissions in activation script

--- backup.nix
+++ backup.nix
@@ -22,13 +22,40 @@ in
 
   users.users = mapAttrs' (n: v: nameValuePair v.user { isSystemUser = true; }) backupsWithUser;
 
-  setupScript = concatStringsSep "\n" (
-    flatten (
-      mapAttrsToList (n: v:
-        map (path: ''
-          ${pkgs.acl.bin}/bin/setfacl -Rm d:u:${v.user}:r,u:${v.user}:r ${path}
-        '') v.paths
-      ) backupsWithUser
-    )
-  );
+  system.activationScripts.backupFilePermissions = {
+    text =
+      let
+        setfacl = "${pkgs.acl.bin}/bin/setfacl";
+        # make sure to run before any other setfacl calls as they are destructive
+        makeAccessible = entity: target: ''
+          current=${target}
+          while current="$(dirname "$current")"; do
+            ${setfacl} --mask -m ${entity}:x "$current" || break
+            if [ "$current" = "/" ]; then
+              break
+            fi
+          done
+        '';
+      in
+        concatStringsSep "\n" (
+          flatten (
+            mapAttrsToList (n: v: [ ''
+                # remove all permissions for this user
+                ${setfacl} -x d:u:${v.user},u:${v.user} /*
+                '' ] ++ [
+                (makeAccessible "u:${v.user}" v.passwordFile) ''
+                ${setfacl} --mask -m u:${v.user}:r ${v.passwordFile}
+                '' ] ++ optional (!(isNull v.s3CredentialsFile)) [
+                  (makeAccessible "u:${v.user}" v.s3CredentialsFile) ''
+                  ${setfacl} --mask -m u:${v.user}:r ${v.s3CredentialsFile}
+                '' ] ++
+              (map (path: ''
+                # TODO: for some reason some files end up with execute bit???
+                ${setfacl} --mask -Rm d:u:${v.user}:rX,u:${v.user}:rX ${path}
+              '') v.paths)
+            ) backupsWithUser
+          )
+        );
+    deps = [];
+  };
 }
