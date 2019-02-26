9886393bf636752e2c5af9f43879cab912281f37
hyperfekt <git@hyperfekt.net>
2019-02-26 16:45:20 +0100

    powersave: disable Ethernet Wake-on-LAN

--- powersave.nix
+++ powersave.nix
@@ -1,4 +1,4 @@
-{lib, ...}:
+{pkgs, lib, ...}:
 {
   powerManagement = {
     cpuFreqGovernor = "powersave";
@@ -10,7 +10,7 @@
     # idle audio card after one second
     "options snd_hda_intel power_save=1"
     # enable wifi power saving (keep uapsd off to maintain low latencies)
-    "options iwlwifi power_save=1 power_leveL=5 d0i3_disable=0 uapsd_disable=1"
+    "options iwlwifi power_save=1 power_level=5 d0i3_disable=0 uapsd_disable=1"
     "options iwldvm force_cam=0"
   ];
 
@@ -19,5 +19,7 @@
     ''ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto"''
     # autosuspend PCI devices
     ''ACTION=="add", SUBSYSTEM=="pci", TEST=="power/control", ATTR{power/control}="auto"''
+    # disable Ethernet Wake-on-LAN
+    ''ACTION=="add", SUBSYSTEM=="net", NAME=="enp*", RUN+="${pkgs.ethtool}/sbin/ethtool -s $name wol d"''
   ];
 }
