a3ecd4bbd2aac7bcf0a5a91ebbf6b047128f25d6
hyperfekt <git@hyperfekt.net>
2019-08-06 16:56:46 +0200

    add aliases

--- aliases.nix
+++ aliases.nix
@@ -1,6 +1,8 @@
 {
   environment.shellAliases = {
-    nix-build-call = "nix-build -E \"with import <nixos-unstable> {}; callPackage ./default.nix {}\"";
+    nix-build-call = ''nix-build -E "with import <nixos> {}; callPackage ./default.nix {}"'';
     nixos-rebuild-unpatched = "nixos-rebuild switch -I nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos";
+    nrs = "sudo nixos-rebuild switch --fast";
+    nru = "nix run -f /nix/var/nix/profiles/per-user/root/channels/nixos-unstable/";
   };
 }
