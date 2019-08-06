901f3efdc181409c4e5f8590da77471801b5d746
hyperfekt <git@hyperfekt.net>
2019-08-06 23:59:08 +0200

    r13y! pin home-manager

--- home-manager.nix
+++ home-manager.nix
@@ -1,21 +1,11 @@
-{user, pkgs, lib, ... }:
+{ user, pkgs, lib, sources, ... }:
 let
-  home-manager-source = (import <nixos/nixpkgs> {}).fetchFromGitHub {
-    owner = "rycee";
-    repo = "home-manager";
-    rev = "9f013a8fb8690d218a4dbefa891faf09c2260a8c";
-    sha256 = "10ncbri6l5a173vzi4fdgdg466dk59xmf28kfwxlfjwrndl4f2y0";
-  };
-
-  home-manager = pkgs.home-manager.overrideAttrs (oldAttrs: {
-    version = "2019-01-28";
-    src = home-manager-source;
-  });
+  home-manager-source = import (import ./nix/sources.nix).home-manager {};
 in
   {
-    imports = [ "${home-manager-source}/nixos" ];
+    imports = [ home-manager-source.nixos ];
 
-    environment.systemPackages = [ home-manager ];
+    environment.systemPackages = [ home-manager-source.home-manager ];
 
     home-manager.users.${user}.home.packages = lib.mkForce [];
   }
