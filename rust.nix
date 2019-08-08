25daa73abc025f8cef88cfed1c64d7bce0d60067
hyperfekt <git@hyperfekt.net>
2019-08-08 05:21:25 +0200

    rust: remove dependency on <nixos>, install rr, switch vscode-lldb to internal library

--- rust.nix
+++ rust.nix
@@ -1,6 +1,6 @@
 { user, pkgs, unstable, ... }:
 let
-  nixpkgs-mozilla = (import <nixos/nixpkgs> {}).fetchFromGitHub {
+  nixpkgs-mozilla = (import (import ./nix/sources.nix).nixpkgs {}).fetchFromGitHub {
     owner = "mozilla";
     repo = "nixpkgs-mozilla";
     # 2019-01-28
@@ -10,15 +10,32 @@ let
 
   rust-mozilla = import "${nixpkgs-mozilla}/rust-overlay.nix";
   rust-src-mozilla = import "${nixpkgs-mozilla}/rust-src-overlay.nix";
+
+  rust = pkgs.latest.rustChannels.nightly.rust;
+  #rust = (pkgs.rustChannelOf { date = "2019-03-13"; channel = "nightly"; }).rust;
 in
 {
   nixpkgs.overlays = [ rust-mozilla rust-src-mozilla ];
 
   environment.systemPackages = [
-    pkgs.latest.rustChannels.nightly.rust
-    pkgs.lldb
+    rust
     unstable.carnix
+    ( unstable.rr.overrideAttrs (oldAttrs: rec {
+      version = "2019-08-03";
+      pname = "rr";
+      name = "${pname}-${version}";
+      src = pkgs.fetchFromGitHub {
+        owner = "mozilla";
+        repo = "rr";
+        rev = "37dfe3320d4dbb0a93810f9178d5fb838e726217";
+        sha256 = "14ynvkrjm70f5jyd993cry1gy0kp7igwaik73m51zb6q79bp1lgr";
+      };
+      buildInputs = with unstable; [ python3Packages.python python3Packages.pexpect ] ++ oldAttrs.buildInputs;
+    } ) )
   ];
 
-  home-manager.users.${user}.programs.vscode.userSettings."rust-client.disableRustup" = true;
+  home-manager.users.${user}.programs.vscode.userSettings = {
+    "rust-client.disableRustup" = true;
+    "lldb.adapterType" = "native";
+  };
 }
