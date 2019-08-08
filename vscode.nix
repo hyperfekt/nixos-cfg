4d46527b3e7703ac58f103bbf44f0d8e836927cd
hyperfekt <git@hyperfekt.net>
2019-08-08 05:34:38 +0200

    vscode: use platform-specific vscode-lldb, install some extensions, use as difftool, fix editor cmdline, make lefthand sides in compares editable

--- vscode.nix
+++ vscode.nix
@@ -22,39 +22,58 @@ let
           bbenoist.Nix
         ] ++ (map idToDrv [
           "rust-lang.rust"
-          "vadimcn.vscode-lldb"
           "pnp.polacode"
           "eamodio.gitlens"
-        ])
-      ;
+          "skyapps.fish-vscode"
+          "GitHub.vscode-pull-request-github"
+          "liximomo.sftp"
+          "MS-vsliveshare.vsliveshare"
+        ]) ++ [ (
+          unstable.vscode-utils.buildVscodeExtension rec {
+            name = "vadimcn.vscode-lldb-x86_64-linux";
+            src = builtins.fetchurl {
+              url = "https://github.com/vadimcn/vscode-lldb/releases/latest/download/vscode-lldb-x86_64-linux.vsix";
+              name = "vadimcn-vscode-lldb-x86_64-linux.zip";
+            };
+            vscodeExtUniqueId = name;
+          }
+        ) ];
     };
 in
   {
     environment.systemPackages = [ custom-vscode ];
 
-    environment.variables."EDITOR" = "${custom-vscode}/bin/code";
+    environment.variables."EDITOR" = "${custom-vscode}/bin/code --wait --new-window";
 
-    home-manager.users.${user}.programs.vscode = {
-      enable = true;
+    home-manager.users.${user}.programs = {
+      git.extraConfig = {
+        diff.tool = "vscode";
+        "difftool \"vscode\"".cmd = "${custom-vscode}/bin/code --wait --new-window --diff $LOCAL $REMOTE";
+      };
 
-      userSettings = {
-        "update.channel" = "none";
-        "extensions.autoUpdate" = false;
-        "editor.wordWrap"= "on";
-        "files.insertFinalNewline" = true;
-        "editor.renderFinalNewline" = false;
-        "files.trimTrailingWhitespace" = true;
-        "[plaintext]" = {
-          "files.insertFinalNewline" = false;
-          "editor.renderFinalNewline" = true;
-          "files.trimTrailingWhitespace" = false;
+      vscode = {
+        enable = true;
+
+        userSettings = {
+          "update.channel" = "none";
+          "extensions.autoUpdate" = false;
+          "editor.wordWrap"= "on";
+          "files.insertFinalNewline" = true;
+          "editor.renderFinalNewline" = false;
+          "files.trimTrailingWhitespace" = true;
+          "[plaintext]" = {
+            "files.insertFinalNewline" = false;
+            "editor.renderFinalNewline" = true;
+            "files.trimTrailingWhitespace" = false;
+          };
+          "[markdown]" = {
+            "files.trimTrailingWhitespace" = false;
+          };
+          "telemetry.enableTelemetry" = false;
+          "telemetry.enableCrashReporter" = false;
+          "[nix]"."editor.tabSize" = 2;
+          "diffEditor.originalEditable"= true;
         };
-        "[markdown]" = {
-          "files.trimTrailingWhitespace" = false;
-        };
-        "telemetry.enableTelemetry" = false;
-        "telemetry.enableCrashReporter" = false;
-        "[nix]"."editor.tabSize" = 2;
       };
     };
   }
