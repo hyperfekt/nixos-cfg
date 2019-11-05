b81f42d373d0d4ecebfd0817ec5a7892e3885c4f
hyperfekt <git@hyperfekt.net>
2019-11-05 17:52:58 +0100

    redshift: adapt to 19.09

--- redshift.nix
+++ redshift.nix
@@ -1,13 +1,12 @@
 { pkgs, ... }:
 {
-  patches = [ (pkgs.fetchpatch {
-    url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/50979.diff";
-    sha256 = "0jq2k3xjk5a8nvbwq5gvhg3z5wf4388lrlzbf3hsfixmzfih6758";
-   }) ];
+  patches = [
+    ./patches/redshift.diff
+  ];
 
   services.redshift = {
     enable = true;
-    config = {
+    settings = {
       redshift = {
         dawn-time = "05:00-06:00";
         dusk-time = "22:00-23:00";
