db4dd0afacdfcb28b819cae0f5df9898785b196c
hyperfekt <git@hyperfekt.net>
2019-11-05 17:50:56 +0100

    vscode: turn into module

--- vscode.nix
+++ vscode.nix
@@ -1,4 +1,5 @@
-{ user, pkgs, unstable, lib, ... }:
+{ user, pkgs, unstable, lib, config, options, ... }:
+with lib;
 let
   idToDrv = id:
     unstable.vscode-utils.buildVscodeExtension {
@@ -9,55 +10,41 @@ let
 
   getVsix = id:
     let
-      publisher = lib.head (lib.splitString "." id);
-      name = lib.head (lib.tail (lib.splitString "." id));
+      publisher = head (splitString "." id);
+      name = head (tail (splitString "." id));
     in
       builtins.fetchurl {
         url = "https://${publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/${publisher}/extension/${name}/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage";
         name = "${publisher}-${name}.zip";
       };
 
-  custom-vscode = unstable.vscode-with-extensions.override {
-      vscodeExtensions = with unstable.vscode-extensions; [
-          bbenoist.Nix
-        ] ++ (map idToDrv [
-          "rust-lang.rust"
-          "pnp.polacode"
-          "eamodio.gitlens"
-          "skyapps.fish-vscode"
-          "GitHub.vscode-pull-request-github"
-          "liximomo.sftp"
-          "MS-vsliveshare.vsliveshare"
-        ]) ++ [ (
-          unstable.vscode-utils.buildVscodeExtension rec {
-            name = "vadimcn.vscode-lldb-x86_64-linux";
-            src = builtins.fetchurl {
-              url = "https://github.com/vadimcn/vscode-lldb/releases/latest/download/vscode-lldb-x86_64-linux.vsix";
-              name = "vadimcn-vscode-lldb-x86_64-linux.zip";
-            };
-            vscodeExtUniqueId = name;
-          }
-        ) ];
-    };
+  customVSCode = unstable.vscode-with-extensions.override {
+    vscodeExtensions = config.vscode.packagedExtensions ++ (map idToDrv config.vscode.pulledExtensions);
+  };
 in
   {
-    environment.systemPackages = [ custom-vscode ];
-
-    environment.variables."EDITOR" = "${custom-vscode}/bin/code --wait --new-window";
-
-    home-manager.users.${user}.programs = {
-      git.extraConfig = {
-        diff.tool = "vscode";
-        "difftool \"vscode\"".cmd = "${custom-vscode}/bin/code --wait --new-window --diff $LOCAL $REMOTE";
+    options.vscode = with types; {
+      packagedExtensions = mkOption {
+        type = listOf package;
+        default = [];
       };
+      pulledExtensions = mkOption {
+        type = listOf string;
+        default = [];
+      };
+      settings = mkOption {
+        type = attrs;
+        default = {};
+      };
+    };
 
-      vscode = {
-        enable = true;
-
-        userSettings = {
-          "update.channel" = "none";
+    config = {
+      vscode.settings = {
+          "update.mode" = "none";
           "extensions.autoUpdate" = false;
-          "editor.wordWrap"= "on";
+      } // {
+          "editor.scrollBeyondLastLine"= false;
+          "editor.wordWrap" = "on";
           "files.insertFinalNewline" = true;
           "editor.renderFinalNewline" = false;
           "files.trimTrailingWhitespace" = true;
@@ -72,7 +59,42 @@ in
           "telemetry.enableTelemetry" = false;
           "telemetry.enableCrashReporter" = false;
           "[nix]"."editor.tabSize" = 2;
-          "diffEditor.originalEditable"= true;
+          "diffEditor.originalEditable" = true;
+          "workbench.sideBar.location" = "right";
+          "editor.acceptSuggestionOnEnter" = "off";
+          "terminal.integrated.automationShell.linux" = pkgs.runCommand "nix-shell_run.sh" { nativeBuildInputs = [ pkgs.makeWrapper ]; }
+            "makeWrapper ${pkgs.nix}/bin/nix-shell $out --run 'shift' --add-flags '--run'"; # replace -c flag with --run flag
+        };
+
+      vscode.pulledExtensions = [
+        "pnp.polacode"
+        "eamodio.gitlens"
+        "GitHub.vscode-pull-request-github"
+        "liximomo.sftp"
+        #"MS-vsliveshare.vsliveshare"
+        "ms-vscode-remote.remote-ssh"
+        "bbenoist.Nix"
+        "arrterian.nix-env-selector"
+        "TabNine.tabnine-vscode"
+      ];
+
+      vscode.packagedExtensions = with unstable.vscode-extensions; [
+        ms-vscode.cpptools
+      ];
+
+      environment.systemPackages = [ customVSCode ];
+
+      environment.variables."EDITOR" = "${customVSCode}/bin/code --wait --new-window";
+
+      home-manager.users.${user}.programs = {
+        git.extraConfig = {
+          diff.tool = "vscode";
+          "difftool \"vscode\"".cmd = "${customVSCode}/bin/code --wait --new-window --diff $LOCAL $REMOTE";
+        };
+
+        vscode = {
+          enable = true;
+          userSettings = config.vscode.settings;
         };
       };
     };
